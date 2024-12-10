543063aa93d4318dce33a2fa7b796008714e5a3b
Flavio <flavio190071@gmail.com>
2024-12-10 14:08:48 +0100

    Add files via upload

---
+++ untitled/dist/script.js
@@ -0,0 +1,29 @@
+function scopri() {
+  // Inserisci il link per "Scoprire"
+  window.location.href = 'https://armundia.com/';
+}
+
+function leggi() {
+  // Inserisci il link per "Leggere"
+  window.location.href = 'https://armundiasrl-my.sharepoint.com/personal/g_citi_armundia_com/_layouts/15/download.aspx?UniqueId=b0555091-c8d8-4d92-a57c-1369f3d17767&Translate=false&tempauth=v1.eyJzaXRlaWQiOiI5OTA5NzY4MS02MmQ0LTRhZGQtOTkzYy03ODU4NDY3ZGE3OGYiLCJhcHBfZGlzcGxheW5hbWUiOiJNaWNyb3NvZnQgVGVhbXMiLCJhcHBpZCI6IjFmZWM4ZTc4LWJjZTQtNGFhZi1hYjFiLTU0NTFjYzM4NzI2NCIsImF1ZCI6IjAwMDAwMDAzLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMC9hcm11bmRpYXNybC1teS5zaGFyZXBvaW50LmNvbUBhNzU5ZmUzYy1iYTEyLTQ1OTAtOWJjNi02YTExMWVhOTM4NGEiLCJleHAiOiIxNzMwMjk1ODc4In0.CgoKBHNuaWQSAjY0EgsIrK2S0cq8vD0QBRoPMTk1LjI1MC4yNTUuMTM5Kix0S3FRcmhHRFVHMTNJZ2VCcytrTUIyOWxXdHB3eVdoUGJoelhXQTkxQ2I0PTCaATgBQhChXv1BZIAAoCY1yK0aSOSlShBoYXNoZWRwcm9vZnRva2VuUhJbImttc2kiLCJkdmNfY21wIl1yKTBoLmZ8bWVtYmVyc2hpcHwxMDAzMjAwMmY0ZTkxN2FkQGxpdmUuY29tegEyggESCTz-WacSupBFEZvGahEeqThKkgEGRmxhdmlvmgEKUGljYXJpZWxsb6IBGWYucGljYXJpZWxsb0Bhcm11bmRpYS5jb22qARAxMDAzMjAwMkY0RTkxN0FEsgFXY29udGFpbmVyLnNlbGVjdGVkIG15ZmlsZXMud3JpdGUgYWxsc2l0ZXMuZnVsbGNvbnRyb2wgYWxsc2l0ZXMubWFuYWdlIGFsbHByb2ZpbGVzLndyaXRlyAEB.e1XPwvfWo5L7fa7A81V0XpP4LQuu2XeaZYJ3mvgSoRw&ApiVersion=2.0';
+}
+
+function guarda() {
+  // Inserisci il link per "Guardare"
+  window.location.href = 'https://youtu.be/Yz7rb0_YNP8';
+}
+
+function linkedin() {
+  // Inserisci il link per il profilo LinkedIn
+  window.location.href = 'https://www.linkedin.com/company/armundia-group/';
+}
+
+function instagram() {
+  // Inserisci il link per il profilo Instagram
+  window.location.href = 'https://www.instagram.com/armundia_group?igsh=eG03ZWZsYW5ob3h5';
+}
+
+function youtube() {
+  // Inserisci il link per il canale YouTube
+  window.location.href = 'https://www.youtube.com/@armundiagroup9701';
+}
